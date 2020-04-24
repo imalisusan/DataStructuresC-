9b704bf6580605b2bf4bc7718e84632d5bfd1c94
Imali Susan <susanimali52@gmail.com>
2020-04-24 09:01:07 -0700

    changes.2

--- question4.cpp
+++ question4.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
+#include<string>
 using namespace std;
 int main() 
 {
+cout<<"Please input a sentence";
+char sentence[100];
+cin>>sentence;
+int check=0, i=0;
+while(sentence[i])
+    {
+        if(check==0)
+        {
+            sentence[i]=toupper(sentence[i]);
+            check=1;
+        }
+        else if(isspace(sentence[i]))
+        {
+            check=0;
+            i++;
+        }
+        cout<<sentence<<endl;
+    }
 
 }
