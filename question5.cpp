393765dd1e99c9c46f11f6ab334918838b7a4523
Imali Susan <susanimali52@gmail.com>
2020-04-24 09:03:37 -0700

    changes.2

--- question5.cpp
+++ question5.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 int main() 
 {
-
+    int area;
 }
+    int rectangle_area()
+    {
+        
+    }
+    int triangle_area()
+    {
+        
+    }
+    int circle_area()
+    {
+        
+    }
+    
