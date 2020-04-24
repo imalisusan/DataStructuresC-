aeda3a77cdfe5e6918ea1ede2dc09c29898f2481
Imali Susan <35591292+imalisusan@users.noreply.github.com>
2020-04-23 18:01:17 -0700

    changes2

--- question1.cpp
+++ question1.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 int main() 
 {
-int number, square;
+int number, square,cube,square_root,cube_root;
+cout << "Please input a number"<<endl;
+cin>>number;
+square= number*number;
+cube= number*number*number;
+square_root=sqrt(number);
+cube_root=cbrt(number);
+cout<< "The square of "<<number<<" is: "<<square<<endl;
+cout<< "The cube of "<<number<<" is: "<<cube<<endl;
+cout<< "The square root of "<<number<<" is: "<<square_root<<endl;
+cout<< "The cube root of "<<number<<" is: "<<cube_root<<endl;
 }
--- question4.cpp
+++ question4.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
+#include<string>
 using namespace std;
 int main() 
 {
+char sentence;
+cout<<"Please input a sentence";
+cin>>sentence;
 
 }
